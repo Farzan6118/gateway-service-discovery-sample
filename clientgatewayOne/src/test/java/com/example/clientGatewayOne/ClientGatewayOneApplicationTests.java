f4c2c74e3f731ce24a4f5d3ab1dea44926fda1ee
farzan.saketi <farzan.saketi@samim.net>
2024-08-19 14:39:49 +0330

    all versions have been upgraded

--- clientgatewayOne/src/test/java/com/example/clientGatewayOne/ClientGatewayOneApplicationTests.java
+++ clientgatewayOne/src/test/java/com/example/clientGatewayOne/ClientGatewayOneApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ClientGatewayOneApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
